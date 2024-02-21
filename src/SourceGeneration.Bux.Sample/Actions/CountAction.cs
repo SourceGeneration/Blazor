ccb96a101da3856ed82af29c116b0588c25c120e
肖昕 <xiao.xin@outlook.com>
2024-02-21 08:23:06 +0100

    Update IStore to State

--- src/SourceGeneration.Bux.Sample/Actions/CountAction.cs
+++ src/SourceGeneration.Bux.Sample/Actions/CountAction.cs
@@ -11,7 +11,7 @@ public class CountAction
 public class DefaultActionHandler
 {
     [ActionHandler]
-    public static void Handle(CountAction _,IStore<MyState> state)
+    public static void Handle(CountAction _, State<MyState> state)
     {
         state.Update(x => x.Count++);
     }
