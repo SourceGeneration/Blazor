c8736ac8016325d1ded8f3c8a73f2a506bb5465c
肖昕 <xiao.xin@outlook.com>
2024-08-22 22:30:07 +0800

    Remove library

--- src/SourceGeneration.Blux/BluxLayoutComponentBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace SourceGeneration.Blux;
-
-public abstract class BluxLayoutComponentBase : BluxComponentBase
-{
-    [Parameter] public RenderFragment? Body { get; set; }
-}
